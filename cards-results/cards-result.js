56d4d05a040f0520032b6a4cdc1de5c3525171d3
Peter Kang <pxk7062@rit.edu>
2024-03-31 10:12:22 -0400

    Rotate instead of slip

--- cards-results/cards-result.js
+++ cards-results/cards-result.js
@@ -3,7 +3,7 @@ function createCard(cardSrc, isFlipped) {
     const card = document.createElement("img");
     let style = "width:18%;padding:5px;min-width: 250px;";
     if (isFlipped) {
-      style = style.concat("transform: scaleY(-1);");
+      style = style.concat("transform: rotate(180deg)");
     }
     card.setAttribute("src", cardSrc ? cardSrc : "cardBack.png");
     card.style = style;
