46b41be28dca2a1dbcb722e385d02087aced9c3b
Peter Kang <pxk7062@rit.edu>
2024-03-30 23:26:45 -0400

    Added Flipped Cards

--- cards-results/cards-result.js
+++ cards-results/cards-result.js
@@ -1,9 +1,9 @@
-function createCard(cardSrc, isFlipped = false) {
+function createCard(cardSrc, isFlipped) {
   if (cardSrc != null) {
     const card = document.createElement("img");
-    const style = "width:18%;padding:5px;min-width: 250px;";
+    let style = "width:18%;padding:5px;min-width: 250px;";
     if (isFlipped) {
-      style.concat("transform: scaleY(-1);");
+      style = style.concat("transform: scaleY(-1);");
     }
     card.setAttribute("src", cardSrc ? cardSrc : "cardBack.png");
     card.style = style;
