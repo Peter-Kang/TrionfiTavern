f50828c6db5997d3048e4ec2ed67a8f62f3a54e0
Peter Kang <pxk7062@rit.edu>
2024-03-30 22:38:41 -0400

    Added bool for flipped cards

--- cards-results/cards-result.js
+++ cards-results/cards-result.js
@@ -1,7 +1,10 @@
-function createCard(cardSrc) {
+function createCard(cardSrc, isFlipped = false) {
   if (cardSrc != null) {
     const card = document.createElement("img");
-    const style = "width:18%;padding:5px;min-width: 250px;"; //transform: scaleY(-1)
+    const style = "width:18%;padding:5px;min-width: 250px;";
+    if (isFlipped) {
+      style.concat("transform: scaleY(-1);");
+    }
     card.setAttribute("src", cardSrc ? cardSrc : "cardBack.png");
     card.style = style;
     return card;
