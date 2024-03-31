0ee3abc33a708131cd5214a4870ae75185e673f3
Peter Kang <pxk7062@rit.edu>
2024-03-30 22:34:15 -0400

    Updated cards

--- cards-results/cards-result.js
+++ cards-results/cards-result.js
@@ -1,7 +1,7 @@
 function createCard(cardSrc) {
   if (cardSrc != null) {
     const card = document.createElement("img");
-    const style = "width:18%;padding:5px;min-width: 250px;";
+    const style = "width:18%;padding:5px;min-width: 250px;"; //transform: scaleY(-1)
     card.setAttribute("src", cardSrc ? cardSrc : "cardBack.png");
     card.style = style;
     return card;
